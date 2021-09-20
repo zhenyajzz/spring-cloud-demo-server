0e045e21501440a7fb375ad6f954b8d12834e80b
ZhenyaGleen <yauheni.hancharou@inbox.ru>
2021-09-20 23:06:30 +0300

    Initial commit

---
+++ src/test/java/com/example/springclouddemoserver/SpringCloudDemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springclouddemoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDemoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
